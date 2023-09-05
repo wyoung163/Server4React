edc1981db3ce3a2712343954ee57e54212017f6c
wyoung163 <20201014@sungshin.ac.kr>
2023-09-05 21:28:57 +0900

    docs: add dto of new user request

---
+++ src/main/java/com/websocket/push/Dto/UserReq.java
@@ -0,0 +1,11 @@
+package com.websocket.push.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserReq {
+    public String email;
+    public String password;
+}
